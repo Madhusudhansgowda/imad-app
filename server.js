3b8d26c835cbf0aab38a51b60381315c23ab90e4
Madhusudhansgowda <30794551+Madhusudhansgowda@users.noreply.github.com>
2017-08-13 13:10:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/another-one',function (req,res){
-    res.send("another page server");
+    res.sendFile(path.join(__dirname, 'ui', 'another-one.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
