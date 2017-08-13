a2b681595761a2701018935c63e2baa615ff0e84
Madhusudhansgowda <30794551+Madhusudhansgowda@users.noreply.github.com>
2017-08-13 15:37:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,44 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone={
+    title:"Another-one",
+    heading:"page content first",
+    content:`There are a number of implementations of the BeanFactory interface that are come straight out-of-the-box with Spring. The most commonly used BeanFactory implementation is the XmlBeanFactory class. This container reads the configuration metadata from an XML file and uses it to create a fully configured system or application.
+
+    There are a number of implementations of the BeanFactory interface that are come straight out-of-the-box with Spring. The most commonly used BeanFactory implementation is the XmlBeanFactory class. This container reads the configuration metadata from an XML file and uses it to create a fully configured system or application.
+    `
+};
+function createtemplate(data){
+var title=data.title;
+var heading=data.heading;
+var content=data.heading;
+var htmlTemplate=`<html>
+<head>
+    <title>
+       $(title)
+    </title>
+    <link href="/ui/style.css" rel="stylesheet" />
+    <style>
+       
+    </style>
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href='/'>home</a>
+        </div>
+        <div>
+            $(heading)
+        </div>
+        <div>
+            $(content)
+        </div>
+        </div>
+    </body>
+</html>
+`;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
@@ -16,7 +54,7 @@ app.get('/another-two',function (req,res){
     res.sendFile(path.join(__dirname, 'ui', 'another-two.html'));
 });
 app.get('/another-one',function (req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'another-one.html'));
+    res.send(createtemplate(anotherone));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
