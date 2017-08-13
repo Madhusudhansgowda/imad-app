fe08b759e47554d6ea95ca6854cc2150b7e2ac8e
Madhusudhansgowda <30794551+Madhusudhansgowda@users.noreply.github.com>
2017-08-13 16:05:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/:articleName',function (req,res){
-    var articleName=re.params.articleName;
+    var articleName=req.params.articleName;
      res.send(createtemplate(articles[articleName]));
 });
 
