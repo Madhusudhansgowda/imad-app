d5761a40f7b89fb92d21d8299f44acc2f039b35c
Madhusudhansgowda <30794551+Madhusudhansgowda@users.noreply.github.com>
2017-08-26 16:01:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,22 +18,37 @@ button.onclick=function()
         }
     };
     
-    
+    request.open('GET','http://madhusudhansgowda123.imad.hasura-app.io/counter',true);
+    request.send(null);
 };
 var inputvalue=document.getElementById("name");
 var vlue=inputvalue.value;
 var submit=document.getElementById("submit");
 submit.onclick=function(){
     
-    var names=request.responseText;
+   
     names=JSON.parse(names);
-    var list='';
+    
+     var request=XMLHttpRequest();
+    request.onreadystatuschange=function()
+    {
+        if(request.readyState==XMLHttpRequest.Done)
+        {
+            if(request.readyState==200)
+            {
+                 var names=request.responseText;
+                 names=JSON.parse(names);
+                var list='';
     for(var i=0;i<names.length;i++)
     {
         list+='<li>'+names[i]+'</li>';
     }
     var ul=document.getElementById("namelist");
     ul.innerHTML=list;
+            }
+        }
+    };
+    
 };
 request.open('GET','http://madhusudhansgowda123.imad.hasura-app.io/dubmit-name?name='+vlue,true);
     request.send(null);
