0cfd9528c72e1c2272d3b0e97b36f293677d1bfc
Madhusudhansgowda <30794551+Madhusudhansgowda@users.noreply.github.com>
2017-08-26 15:04:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,7 +3,21 @@ var button=document.getElementById("button");
 var counter=0;
 button.onclick=function()
 {
-    counter=counter+1;
-    var span=document.getElementById("btn");
-    span.innerHTML=counter.toString();
+    
+    var request=XMLHttpRequest();
+    request.onreadystatuschange=function()
+    {
+        if(request.readyState==XMLHttpRequest.Done)
+        {
+            if(request.readyState==200)
+            {
+                var counter=request.responseText;
+                var span=document.getElementById("btn");
+                span.innerHTML=counter.toString();
+            }
+        }
+    };
+    request.open('GET','http://madhusudhansgowda123.imad.hasura-app.io');
+    request.send(null);
+    
 };
