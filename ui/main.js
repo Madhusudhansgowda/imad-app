4ba1f97b8ca9ad4c5189059de65512802a2e06de
Madhusudhansgowda <30794551+Madhusudhansgowda@users.noreply.github.com>
2017-08-26 15:27:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ var vlue=inputvalue.value;
 var submit=document.getElementById("submit");
 submit.onclick=function(){
     
-    var names=["name","name","name"];
+    var names=['name1','name2','name3'];
     var list='';
     for(var i=0;i<names.length;i++)
     {
