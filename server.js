3f647cdc9f315432c032de060c03f8136d836d3c
Madhusudhansgowda <30794551+Madhusudhansgowda@users.noreply.github.com>
2017-08-26 15:43:52 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -82,7 +82,7 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 var names=[];
-app.get('/submit-name', function(req,res){
+app.get('/submit-name/:name', function(req,res){
     var name=req.params.name;
     req.push(names);
     req.send(JSON.stringfy(names));
