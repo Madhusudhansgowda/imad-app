01ce019f2754ebe3597d75e6deff747c0161971e
Madhusudhansgowda <30794551+Madhusudhansgowda@users.noreply.github.com>
2017-08-26 13:56:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
+
+var counter=0;
+app.get('/counter',function(req,res){
+    counter+=1;
+    res.send(counter.toString());
+    
+});
 var articles={
     
 'another-one':{
@@ -58,12 +65,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-var counter=0;
-app.get('/counter',function(req,res){
-    counter+=1;
-    res.send(counter.toString());
-    
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
